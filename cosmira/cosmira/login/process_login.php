8d665a321214c505d3e65f7c663a60e0ef9d9386
vyeung7 <123509757+vyeung7@users.noreply.github.com>
2025-01-30 20:17:24 -0500

    new

---
+++ cosmira/cosmira/login/process_login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE php>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Cosmira</title>
+    <!-- need to link js and css pages -->
+    <script src="js/cosmira.js"></script>
+    <link rel="stylesheet" href="login.css" />
+  </head>
+  <body>
+    <header>
+      <div class="heading">
+        <div class="logo">
+          <img src="../assests/img/cos-removebg.png" alt="Cosmira Logo" />
+        </div>
+        <div class="search">
+          <input type="text" placeholder="S E A R C H" />
+        </div>
+      </div>
+      <nav>
+        <ul class="nav-links">
+          <li><a class="nav-link" href="about.html">ABOUT</a></li>
+          <li><a class="nav-link" href="quiz.html">QUIZ</a></li>
+          <li><a class="nav-link" href="discover.html">DISCOVER</a></li>
+          <li><a class="nav-link" href="community.html">COMMUNITY</a></li>
+          <li><a class="nav-link" href="profile.html">PROFILE</a></li>
+        </ul>
+      </nav>
+    </header>
+
+    <h1>Currently Under Construction!</h1>
+    <a href="../../index.html"> Click here to exit</a>
+
+
+    </body>
+</html>
